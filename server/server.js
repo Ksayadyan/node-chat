24bf943c525b74c9aca152f17fd6529e7c28e75a
Ksayadyan <38043223+Ksayadyan@users.noreply.github.com>
2018-08-20 22:45:29 +0400

    'init'

--- server/server.js
+++ server/server.js
@@ -15,6 +15,19 @@ app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
   console.log('User is connected');
+
+  socket.emit('newMessage',{
+    from:'Admin',
+    text:'Welcome to chat',
+    createdAt: new Date().getTime(),
+  });
+
+  socket.broadcast.emit('newMessage',{
+    from:'Admin',
+    text:'New user connected',
+    createdAt: new Date().getTime(),
+  })
+
   socket.on('disconnect',()=>{
     console.log('User disconnected');
   });
@@ -26,6 +39,11 @@ io.on('connection',(socket)=>{
       text: message.text,
       createdAt: new Date().getTime(),
     });
+    // socket.broadcast.emit('newMessage',{
+    //   from: message.from,
+    //   text : message.text,
+    //   createdAt: new Date().getTime(),
+    //})
   });
 })
 

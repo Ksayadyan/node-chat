91b4dd8d524758b7779879da5d2e2f59554ecfd7
Ksayadyan <38043223+Ksayadyan@users.noreply.github.com>
2018-08-20 22:28:38 +0400

    'init'

--- server/server.js
+++ server/server.js
@@ -15,11 +15,21 @@ app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
   console.log('User is connected');
-
   socket.on('disconnect',()=>{
     console.log('User disconnected');
-  })
+  });
+
+  socket.on('createMessage',(message)=>{
+    console.log('createMessage',message)
+    io.emit('newMessage',{
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime(),
+    });
+  });
 })
+
+
 
 
 

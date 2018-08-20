21a9f9e69075209bdceb4193e4339da6f21b7793
Ksayadyan <38043223+Ksayadyan@users.noreply.github.com>
2018-08-20 20:04:38 +0400

    'init'

--- server/server.js
+++ server/server.js
@@ -7,6 +7,6 @@ const port = process.env.PORT || 3000;
 let app = express();
 app.use(express.static(publicPath));
 
-app.listen(PORT,()=>{
+app.listen(port,()=>{
   console.log('Server is up on port 3000');
 })
